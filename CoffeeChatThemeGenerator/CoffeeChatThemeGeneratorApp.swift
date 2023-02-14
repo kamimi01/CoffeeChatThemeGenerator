6b203b86b4038a71d73aff2103fc2eb258109fbe
mika.urakawa <awakaru0122@gmail.com>
2023-02-14 09:52:30 +0900

    add: first project

---
+++ CoffeeChatThemeGenerator/CoffeeChatThemeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeChatThemeGeneratorApp.swift
+//  CoffeeChatThemeGenerator
+//
+//  Created by mikaurakawa on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeChatThemeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

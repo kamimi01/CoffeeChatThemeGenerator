b40744e1390b6bde6a98c89a86720f54b8765cd9
mika.urakawa <awakaru0122@gmail.com>
2023-02-19 14:40:28 +0900

    add: theme result screen

--- CoffeeChatThemeGenerator/ThemeResult/ThemeResultScreen.swift
+++ CoffeeChatThemeGenerator/ThemeResult/ThemeResultScreen.swift
@@ -11,7 +11,34 @@ struct ThemeResultScreen: View {
     let theme: String
 
     var body: some View {
-        Text(theme)
+        VStack(alignment: .leading, spacing: 20) {
+            Text("こんなテーマで話しましょう")
+                .font(.title2)
+                .foregroundColor(.smokeBlack)
+                .bold()
+            Text(theme)
+                .font(.title3)
+                .foregroundColor(.smokeBlack)
+        }
+        .padding(.horizontal, 16)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                shareButton
+            }
+        }
+    }
+}
+
+private extension ThemeResultScreen {
+    var shareButton: some View {
+        ShareLink(item: sharedText) {
+            Image(systemName: "square.and.arrow.up")
+        }
+        .foregroundColor(.smokeBlack)
+    }
+
+    var sharedText: String {
+        return "話したいことは、\n\(theme)\n\ncreated by テーマジェネレーター"
     }
 }
 

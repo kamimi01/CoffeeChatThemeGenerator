6bca83e6be1d7524e3b7d09e8dce61297474694f
mika.urakawa <awakaru0122@gmail.com>
2023-02-19 15:17:06 +0900

    add: setting view

---
+++ CoffeeChatThemeGenerator/Setting/SettingScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SettingScreenn.swift
+//  CoffeeChatThemeGenerator
+//
+//  Created by mikaurakawa on 2023/02/19.
+//
+
+import SwiftUI
+
+struct SettingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingScreenn_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingScreen()
+    }
+}
